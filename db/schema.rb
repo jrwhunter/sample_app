52000a36b0e9e336a068503be453aaf0120d9e48
jrwhunter <j.hunter@abdn.ac.uk>
2014-03-02 16:34:57 +0000

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140228195212) do
+ActiveRecord::Schema.define(version: 20140302152743) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -23,8 +23,10 @@ ActiveRecord::Schema.define(version: 20140228195212) do
     t.boolean  "visible"
     t.string   "preferences"
     t.string   "favourites"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
